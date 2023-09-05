791741499abfe19036e8fd3d11353689afec41c3
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-09-04 19:42:06 -0500

    have the prompt button working to build new grid

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const gridContainer = document.querySelector(".grid-container");
+const resizeBtn = document.getElementById("resize");
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.addEventListener("mouseover", () => {
-    square.style.background = "pink";
-  });
-  gridContainer.appendChild(square);
+let squareNum = 256;
+
+resizeBtn.addEventListener("click", () => {
+  const resizeNum = Number(
+    prompt("Enter a new size for grid (16 = 16x16, 32 = 32x32, etc...)"),
+  );
+
+  gridContainer.innerHTML = "";
+
+  buildGrid(resizeNum * resizeNum);
+});
+
+function buildGrid(squareNum) {
+  for (let i = 0; i < squareNum; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.addEventListener("mouseover", () => {
+      square.style.background = "pink";
+    });
+    gridContainer.appendChild(square);
+  }
 }
+
+buildGrid(squareNum);
