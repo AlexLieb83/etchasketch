4d44d81062efdb6fcdc4c326d95bb9c3e3f50dbf
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-09-04 19:23:09 -0500

    added background fill to each square

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const gridContainer = document.querySelector(".grid-container");
 for (let i = 0; i < 256; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
+  square.addEventListener("mouseover", () => {
+    square.style.background = "pink";
+  });
   gridContainer.appendChild(square);
 }
